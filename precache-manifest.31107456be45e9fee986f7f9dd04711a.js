5f7cfb6af67e349aff8415e23d6fb9150f912950
Nguyen Huu <nguyencules@gmail.com>
2020-05-29 19:33:50 +0700

    Updates

---
+++ precache-manifest.31107456be45e9fee986f7f9dd04711a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbe10514ac27ab5df7aa48fbe7a095c",
+    "url": "/demo_api/index.html"
+  },
+  {
+    "revision": "4181040aa609fa00be44",
+    "url": "/demo_api/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "44bc9367670d5db1c6e1",
+    "url": "/demo_api/static/css/main.e5ed62a5.chunk.css"
+  },
+  {
+    "revision": "4181040aa609fa00be44",
+    "url": "/demo_api/static/js/2.254fc72d.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/demo_api/static/js/2.254fc72d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44bc9367670d5db1c6e1",
+    "url": "/demo_api/static/js/main.ccdeed67.chunk.js"
+  },
+  {
+    "revision": "1078acde69cf9c6284c5",
+    "url": "/demo_api/static/js/runtime-main.dd7b28ea.js"
+  },
+  {
+    "revision": "7c18188784f21915f42a5b3bc9d91e20",
+    "url": "/demo_api/static/media/Roboto-Bold.7c181887.ttf"
+  },
+  {
+    "revision": "d52f011be65b281ba8ca1c3f689cf133",
+    "url": "/demo_api/static/media/Roboto-Medium.d52f011b.ttf"
+  },
+  {
+    "revision": "18d44f79b3979ec168862093208c6d7d",
+    "url": "/demo_api/static/media/Roboto-Regular.18d44f79.ttf"
+  },
+  {
+    "revision": "68a4fc0a24563d2d333dedd2a1b52ffd",
+    "url": "/demo_api/static/media/blue.68a4fc0a.png"
+  },
+  {
+    "revision": "b955840bd1ce3edd2fd72889d9574c78",
+    "url": "/demo_api/static/media/green.b955840b.png"
+  },
+  {
+    "revision": "2a83e095deb24d0b716fd2e4f87cdb32",
+    "url": "/demo_api/static/media/orange.2a83e095.png"
+  },
+  {
+    "revision": "df7762ee7c8f3cdb8d40289ae8a45ece",
+    "url": "/demo_api/static/media/red.df7762ee.png"
+  },
+  {
+    "revision": "ac4b5bfbfdf5e057c65574cf37e0c8e4",
+    "url": "/demo_api/static/media/yellow.ac4b5bfb.png"
+  }
+]);
